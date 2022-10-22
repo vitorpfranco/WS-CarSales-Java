05c732d0c2f370b2073b933107e4dc3edd46de19
vitorpfranco <vitor.pereira.franco@gmail.com>
2022-10-22 20:30:34 -0300

    Template

---
+++ src/main/java/com/api/WSCarSales/WsCarSalesApplication.java
@@ -0,0 +1,13 @@
+package com.api.WSCarSales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsCarSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsCarSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/WSCarSales/WsCarSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.WSCarSales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsCarSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
